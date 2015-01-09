16a4a42d12164b414267615404207f1b61e0e80a
saebuabu <a.saebu@outlook.com>
2015-01-09 11:19:09 +0100

    eerste opdracht voor php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Opdrachten overzicht PHP periode 3</title>
+    </head>
+    <body>
+        <h1>Opdrachten overzicht PHP periode 3</h1>
+        <ul>    
+            <li><a href="opdracht001">Opdracht 001</a></li>
+        </ul>
+        
+    </body>
+</html>
---
+++ opdracht001/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Opdracht001</title>
+    </head>
+    <body>
+        <h1>Opdracht 001</h1>
+        <p>Maak hieronder je eerste php script; print hallo wereld in deze pagina.</p>
+        <hr>
+        <a href="../index.php">Terug</a>
+    </body>
+</html>
