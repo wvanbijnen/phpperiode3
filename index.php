69eb5bc8491448288fe6979f37614890fda57491
saebuabu <a.saebu@outlook.com>
2015-01-09 11:31:18 +0100

    Opdracht twee is toegevoegd en de naamgeving van de bestanden

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
     <body>
         <h1>Opdrachten overzicht PHP periode 3</h1>
         <ul>    
-            <li><a href="opdracht001">Opdracht 001</a></li>
-        </ul>
-        
+            <li><a href="opdracht001/opdracht001.php">Opdracht001</a></li>
+            <li><a href="opdracht002/opdracht002.php">Opdracht002</a></li>
+        </ul>    
     </body>
 </html>
---
+++ opdracht002/opdracht002.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        <h1>Opdracht 002</h1>
+        <p>Declareer variabelen voor iedere leerling in je klas...</p>
+        <a href="../index.php">Terug</a>
+    </body>
+</html>
