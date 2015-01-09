8efcababcd399640c1be5f5c56c5f029996d895d
saebuabu <a.saebu@outlook.com>
2015-01-09 12:45:53 +0100

    styling toegevoegd aan index.php

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <?php
-
+    
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <link type="text/css" rel="stylesheet" href="css/style.css">
         <meta charset="utf-8" />
         <title>Opdrachten overzicht PHP periode 3</title>
     </head>
     <body>
-        <h1>Opdrachten overzicht PHP periode 3</h1>
+        <header><h1>Uitwerking van PHP-opdrachten van [Je naam]</h1></header>
+        <div id="wrapper">
+        <h2>Overzicht PHP periode 3</h2>
         <ul>    
             <li><a href="opdracht001/opdracht001.php">Opdracht001</a></li>
             <li><a href="opdracht002/opdracht002.php">Opdracht002</a></li>
+            <li><a href="opdracht002/opdracht003.php">Opdracht003</a></li>
         </ul>    
+        </div>
     </body>
 </html>
